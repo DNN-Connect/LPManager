6b1b6b7008008e982bd5017cc12c231620f06f67
donker <none@none.com>
2009-08-09 21:52:39 +0000

    Handling resource files in uploaded packages. Moved manifest reading logic to new file 'Helpers/ManifestReader'

--- DNNEurope.LocalizationEditor/Components/Helpers/ZipHelper.vb
+++ DNNEurope.LocalizationEditor/Components/Helpers/ZipHelper.vb
@@ -20,35 +20,43 @@ Imports System.IO
 Imports ICSharpCode.SharpZipLib.Zip
 
 Public Class ZipHelper
-    Public Shared Sub Unzip(ByVal fileStream As Stream, ByVal tempDirectory As String)
-        Dim objZipEntry As ZipEntry
-        Using objZipInputStream As New ZipInputStream(fileStream)
-            objZipEntry = objZipInputStream.GetNextEntry
-            While Not objZipEntry Is Nothing
-                Dim strFileName As String = objZipEntry.Name.Replace("/", "\")
-                If strFileName <> "" And Not objZipEntry.IsDirectory Then
-                    Dim sFile As String = strFileName
-                    Dim sPath As String = tempDirectory & "\"
-                    If strFileName.IndexOf("\"c) > 0 Then
-                        sFile = Mid(strFileName, strFileName.LastIndexOf("\"c) + 2)
-                        sPath = sPath & Left(strFileName, strFileName.LastIndexOf("\"c))
-                        If Not Directory.Exists(sPath) Then
-                            Directory.CreateDirectory(sPath)
-                        End If
-                        sPath &= "\"
-                    End If
-                    Using objFileStream As FileStream = File.Create(sPath & sFile)
-                        Dim intSize As Integer = 2048
-                        Dim arrData(2048) As Byte
-                        intSize = objZipInputStream.Read(arrData, 0, arrData.Length)
-                        While intSize > 0
-                            objFileStream.Write(arrData, 0, intSize)
-                            intSize = objZipInputStream.Read(arrData, 0, arrData.Length)
-                        End While
-                    End Using
-                End If
-                objZipEntry = objZipInputStream.GetNextEntry
-            End While
-        End Using
-    End Sub
+
+ Public Shared Sub Unzip(ByVal fileStream As Stream, ByVal tempDirectory As String)
+  Dim objZipEntry As ZipEntry
+  Using objZipInputStream As New ZipInputStream(fileStream)
+   objZipEntry = objZipInputStream.GetNextEntry
+   While Not objZipEntry Is Nothing
+    Dim strFileName As String = objZipEntry.Name.Replace("/", "\")
+    If strFileName <> "" And Not objZipEntry.IsDirectory Then
+     Dim sFile As String = strFileName
+     Dim sPath As String = tempDirectory & "\"
+     If strFileName.IndexOf("\"c) > 0 Then
+      sFile = Mid(strFileName, strFileName.LastIndexOf("\"c) + 2)
+      sPath = sPath & Left(strFileName, strFileName.LastIndexOf("\"c))
+      If Not Directory.Exists(sPath) Then
+       Directory.CreateDirectory(sPath)
+      End If
+      sPath &= "\"
+     End If
+     Using objFileStream As FileStream = File.Create(sPath & sFile)
+      Dim intSize As Integer = 2048
+      Dim arrData(2048) As Byte
+      intSize = objZipInputStream.Read(arrData, 0, arrData.Length)
+      While intSize > 0
+       objFileStream.Write(arrData, 0, intSize)
+       intSize = objZipInputStream.Read(arrData, 0, arrData.Length)
+      End While
+     End Using
+    End If
+    objZipEntry = objZipInputStream.GetNextEntry
+   End While
+  End Using
+ End Sub
+
+ Public Shared Sub Unzip(ByVal filePath As String, ByVal tempDirectory As String)
+  Using fileStrm As IO.FileStream = File.Open(filePath, FileMode.Open, FileAccess.Read)
+   Unzip(fileStrm, tempDirectory)
+  End Using
+ End Sub
+
 End Class
