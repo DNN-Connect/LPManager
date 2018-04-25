d69d0b89daa197ab6d1ae5f05b4e65ad2dd25aea
Peter Donker <peter@bring2mind.net>
2018-04-25 09:19:47 +0200

    Release

--- My Project/AssemblyInfo.vb
+++ My Project/AssemblyInfo.vb
@@ -27,9 +27,9 @@ Imports System.Runtime.InteropServices
 ' You can specify all the values or you can default the Build and Revision Numbers 
 ' by using the '*' as shown below:
 
-<Assembly: AssemblyVersion("04.05.01")> 
-<Assembly: AssemblyFileVersionAttribute("04.05.01")> 
-<Assembly: AssemblyInformationalVersion("04.05.01")> 
+<Assembly: AssemblyVersion("04.05.02")> 
+<Assembly: AssemblyFileVersionAttribute("04.05.02")> 
+<Assembly: AssemblyInformationalVersion("04.05.02")> 
 
 'History
 '03.00.00 2009-08-10 Janga: User Interface.
